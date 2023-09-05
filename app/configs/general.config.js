30e5ac3c7465adb98ec9d95c6ad052d84393d49e
LAPTOP-EB1P93V6\Hieu <scottnguyen2411@gmail.com>
2023-09-05 19:36:43 +0700

    Holder

--- app/configs/general.config.js
+++ app/configs/general.config.js
@@ -1,10 +1,10 @@
 module.exports = {
     DBConnectors: {
-        host: process.env.DB_HOST || 'db4free.net',
-        port: process.env.DB_PORT || 3306,
-        username: process.env.DB_USER || 'sheeper',
+        host: process.env.DB_HOST || "127.0.0.1" || 'db4free.net',
+        port: process.env.DB_PORT || 3306 || 443,
+        username: process.env.DB_USER || "root" || 'sheeper',
         password: process.env.DB_PASSWORD || 'H.S20T.H04',
-        database: process.env.DB_NAME || "test_api",
+        database: process.env.DB_NAME || "blog_schema" || "test_api",
         dialect: process.env.DB_DIALECT || "mysql",
     },
     jwtAuthKey: 'abcxyz',
