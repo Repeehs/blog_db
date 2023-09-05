48a090539a9fe2131d86e3e8d41f295cf209ef7f
LAPTOP-EB1P93V6\Hieu <scottnguyen2411@gmail.com>
2023-09-05 18:26:42 +0700

    Deployment test 2

--- app/configs/general.config.js
+++ app/configs/general.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     DBConnectors: {
         host: process.env.DB_HOST || 'db4free.net',
-        port: process.env.DB_PORT || 443,
+        port: process.env.DB_PORT || 3306,
         username: process.env.DB_USER || 'sheeper',
         password: process.env.DB_PASSWORD || 'H.S20T.H04',
         database: process.env.DB_NAME || "test_api",
