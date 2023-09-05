ffde43f69d4fcd3b29844e0b7b2feee8fa8ca98d
LAPTOP-EB1P93V6\Hieu <scottnguyen2411@gmail.com>
2023-09-05 14:29:32 +0700

    deploy upload checking

--- app/configs/general.config.js
+++ app/configs/general.config.js
@@ -1,10 +1,10 @@
 module.exports = {
     DBConnectors: {
-        host: process.env.DB_HOST || '127.0.0.1',
-        port: process.env.DB_PORT || 3306,
-        username: process.env.DB_USER || 'root',
-        password: process.env.DB_PASSWORD || '',
-        database: process.env.DB_NAME || "blog_schema",
+        host: process.env.DB_HOST || 'db4free.net',
+        port: process.env.DB_PORT || 443,
+        username: process.env.DB_USER || 'sheeper',
+        password: process.env.DB_PASSWORD || 'H.S20T.H04',
+        database: process.env.DB_NAME || "test_api",
         dialect: process.env.DB_DIALECT || "mysql",
     },
     jwtAuthKey: 'abcxyz',
