93f6a0cb133cbb0f1698e5b825fd0769026376d7
LAPTOP-EB1P93V6\Hieu <scottnguyen2411@gmail.com>
2023-09-06 08:47:06 +0700

    ughhhh

--- app/configs/general.config.js
+++ app/configs/general.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     DBConnectors: {
 <<<<<<< HEAD
+<<<<<<< HEAD
         host: process.env.DB_HOST || "127.0.0.1" || 'db4free.net',
         port: process.env.DB_PORT || 3306 || 443,
         username: process.env.DB_USER || "root" || 'sheeper',
@@ -12,6 +13,16 @@ module.exports = {
     tokenLoginExpireDays: '30 days'
 };
 // Im tired
+=======
+        host: process.env.DB_HOST || 'db4free.net',
+        port: process.env.DB_PORT || 3306,
+        username: process.env.DB_USER || 'sheeper',
+        password: process.env.DB_PASSWORD || 'H.S20T.H04',
+        database: process.env.DB_NAME || "blog_schema",
+        dialect: process.env.DB_DIALECT || "mysql",
+    },
+};
+>>>>>>> 4d4333b (Deploy test (again))
 =======
         host: process.env.DB_HOST || 'db4free.net',
         port: process.env.DB_PORT || 3306,
