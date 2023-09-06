4d4333bb357ed1e5e53cdcf590673840b194a232
LAPTOP-EB1P93V6\Hieu <scottnguyen2411@gmail.com>
2023-09-06 07:43:57 +0700

    Deploy test (again)

--- app/configs/general.config.js
+++ app/configs/general.config.js
@@ -1,12 +1,10 @@
 module.exports = {
     DBConnectors: {
         host: process.env.DB_HOST || 'db4free.net',
-        port: process.env.DB_PORT || 443,
+        port: process.env.DB_PORT || 3306,
         username: process.env.DB_USER || 'sheeper',
         password: process.env.DB_PASSWORD || 'H.S20T.H04',
-        database: process.env.DB_NAME || "test_api",
+        database: process.env.DB_NAME || "blog_schema",
         dialect: process.env.DB_DIALECT || "mysql",
     },
-    jwtAuthKey: 'abcxyz',
-    tokenLoginExpireDays: '30 days'
 };
