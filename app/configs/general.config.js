9c11a5085451021f7f5f6a11446e01c6ce4ad0f5
LAPTOP-EB1P93V6\Hieu <scottnguyen2411@gmail.com>
2023-09-06 08:40:28 +0700

    ?

--- app/configs/general.config.js
+++ app/configs/general.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     DBConnectors: {
+<<<<<<< HEAD
         host: process.env.DB_HOST || "127.0.0.1" || 'db4free.net',
         port: process.env.DB_PORT || 3306 || 443,
         username: process.env.DB_USER || "root" || 'sheeper',
@@ -11,3 +12,13 @@ module.exports = {
     tokenLoginExpireDays: '30 days'
 };
 // Im tired
+=======
+        host: process.env.DB_HOST || 'db4free.net',
+        port: process.env.DB_PORT || 3306,
+        username: process.env.DB_USER || 'sheeper',
+        password: process.env.DB_PASSWORD || 'H.S20T.H04',
+        database: process.env.DB_NAME || "blog_schema",
+        dialect: process.env.DB_DIALECT || "mysql",
+    },
+};
+>>>>>>> 4d4333b (Deploy test (again))
